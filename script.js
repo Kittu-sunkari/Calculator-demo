9b6c9510db221306c6dc3fc719f730ce365a75fd
kittu-sunkari <ramkrishnasunkari001@gmail.com>
2026-04-14 14:18:15 +0530

    created dynamic calucation

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 let display = document.getElementById('display');
 
-function calculatetor() {
-    return Function('"use strict";return (' + display.value + ')')();
+// function calculatetor() {
+//     return Function('"use strict";return (' + display.value + ')')();
     
-    // let input = display.value; // e.g., "10+5"
+//     // let input = display.value; // e.g., "10+5"
     
-    // let parts = input.split(/([+\-*/%()^])/).filter(part => part.trim() !== '');
-    // if (parts.length !== 3) {
-    //     throw new Error('Invalid expression');
-    // }   
-    // let num1 = parseFloat(parts[0]);
-    // let operator = parts[1];
-    // let num2 = parseFloat(parts[2]);    
-    // // Perform the calculation based on the operator
-    // switch (operator) {
-    //     case '+':   
-    //         return num1 + num2;
-    //     case '-':
-    //         return num1 - num2; 
-    //     case '*':
-    //         return num1 * num2;
-    //     case '/':
-    //         if (num2 === 0) {
-    //             throw new Error('Division by zero');
-    //         }   
-    //         return num1 / num2;
-    //     case '%':
-    //         return num1 % num2;
-    //     case '^':
-    //         return Math.pow(num1, num2);
-    //     default:
-    //         throw new Error('Unsupported operator');
-    // }       
+//     // let parts = input.split(/([+\-*/%()^])/).filter(part => part.trim() !== '');
+//     // if (parts.length !== 3) {
+//     //     throw new Error('Invalid expression');
+//     // }   
+//     // let num1 = parseFloat(parts[0]);
+//     // let operator = parts[1];
+//     // let num2 = parseFloat(parts[2]);    
+//     // // Perform the calculation based on the operator
+//     // switch (operator) {
+//     //     case '+':   
+//     //         return num1 + num2;
+//     //     case '-':
+//     //         return num1 - num2; 
+//     //     case '*':
+//     //         return num1 * num2;
+//     //     case '/':
+//     //         if (num2 === 0) {
+//     //             throw new Error('Division by zero');
+//     //         }   
+//     //         return num1 / num2;
+//     //     case '%':
+//     //         return num1 % num2;
+//     //     case '^':
+//     //         return Math.pow(num1, num2);
+//     //     default:
+//     //         throw new Error('Unsupported operator');
+//     // }       
 
-}
+// }
 
 
 function appendToDisplay(value) {
@@ -47,7 +47,7 @@ function calculate() {
         if(display.value.trim() === '') {   
             display.value = '';
         } else {
-            display.value = calculatetor(display.value);
+            display.value = eval(display.value);
         }
     } catch (error) {
         display.value = 'Error';
